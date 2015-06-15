39a213bb11eb1ff4649c252ffd44969294a5f296
Shaohan Huang <buaahsh@foxmail.com>
2015-06-15 21:20:24 +0800

    find question from query log

--- BaseNLP/QA/BuildSeed.py
+++ BaseNLP/QA/BuildSeed.py
@@ -1,6 +1,7 @@
 __author__ = 'hsh'
+# -*- coding:utf-8 -*-
 
-
+import codecs
 
 class SeedManager():
 
@@ -35,6 +36,36 @@ def ComputeCoverageOfSeed(seedFile, queryFile):
             print query
     print c
 
+def FindQuestion(seedFile, querySet, outputFile):
+    seedManager = SeedManager(seedFile)
+    writer = open(outputFile, "w")
+    reader = open(querySet, "rb")
+    #f = codecs.open(querySet, "r", encoding="utf-8")
+    lineNum = 0
+    for line in reader:
+        query = line.strip().split("\t")[0]
+
+        lineNum += 1
+        if lineNum % 100000 == 0:
+            print lineNum
+        seed = seedManager.HasSeed(query)
+        if seed:
+            writer.write(str.format("{0}\t{1}\n", line.strip(), seed))
+    writer.close()
+
+def FindHighFre(inputFile, outputFile, threshold=1000):
+    reader = open(inputFile, "rb")
+    writer = open(outputFile, "w")
+    for line in reader:
+        freq = int(line.split("\t")[1])
+        if freq > threshold:
+            writer.write(line.strip() + "\n")
 
 if __name__ == "__main__":
-    ComputeCoverageOfSeed("seed", "/Users/hsh/Downloads/NLPCC-2015-OpenQA-TestSet/NLPCC-2015-OpenQA-Questions-Chinese-Formated")
+    seedFile = "seed"
+    querySet = "F:\\query.tsv"
+    outputFile = "F:\\query.tsv.question"
+    outputFile_2 = "F:\\query.tsv.head.question"
+    # ComputeCoverageOfSeed("seed", "/Users/hsh/Downloads/NLPCC-2015-OpenQA-TestSet/NLPCC-2015-OpenQA-Questions-Chinese-Formated")
+    # FindQuestion(seedFile, querySet, outputFile)
+    FindHighFre(outputFile, outputFile_2)
